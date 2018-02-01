02af262fbd7709bd3e180ccf5c9cec6bddc5468b
Sébastien Rancoud <sebastien.rancoud@gmail.com>
2018-02-01 21:46:55 +0100

    first push

---
+++ src/Crypt.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rancoud\Crypt;
+
+/**
+ * Class Crypt.
+ */
+class Crypt
+{
+    /**
+     * Crypt constructor.
+     */
+    public function __construct()
+    {
+    }
+}
