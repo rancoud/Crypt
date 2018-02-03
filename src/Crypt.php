d75f07cfc005fb7931ab7e02fcd160b7f049a9ad
Sébastien Rancoud <sebastien.rancoud@gmail.com>
2018-02-04 00:21:58 +0100

    add bcrypt with fallback when argon2i is not usable

--- src/Crypt.php
+++ src/Crypt.php
@@ -4,29 +4,77 @@ declare(strict_types=1);
 
 namespace Rancoud\Crypt;
 
+use Exception;
+
 /**
  * Class Crypt.
  */
 class Crypt
 {
-    protected static $algo = PASSWORD_ARGON2I;
+    protected static $algoBcrypt = 1; // PASSWORD_BCRYPT
+    protected static $algoArgon2i = 2; // PASSWORD_ARGON2I
 
-    protected static $options = [
-        'memory_cost' => 1 << 17, // 128 Mb
-        'time_cost'   => 4,
-        'threads'     => 3,
+    protected static $algoCurrent = 2; // by default use ARGON2I
+    protected static $algoFixed = false;
+
+    protected static $optionsArgon2i = [
+        'memory_cost' => 1024, // PASSWORD_ARGON2_DEFAULT_MEMORY_COST
+        'time_cost'   => 2, // PASSWORD_ARGON2_DEFAULT_TIME_COST
+        'threads'     => 2, // PASSWORD_ARGON2_DEFAULT_THREADS
     ];
 
-    protected static $caracters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    protected static $optionsBcrypt = [
+        'cost ' => 12,
+    ];
+
+    protected static $caracters = '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmno
+pqrstuvwxyz{|}~';
+
+    public static function useArgon2i(): void
+    {
+        static::$algoFixed = true;
+
+        static::$algoCurrent = static::$algoArgon2i;
+    }
+
+    public static function useBcrypt(): void
+    {
+        static::$algoFixed = true;
+
+        static::$algoCurrent = static::$algoBcrypt;
+    }
 
     /**
      * @param string $password
+     *
+     * @throws Exception
      *
      * @return string
      */
     public static function hash(string $password): string
     {
-        return password_hash($password, static::$algo, static::$options);
+        $string = null;
+
+        static::chooseAlgo();
+
+        try {
+            if (static::$algoCurrent === static::$algoArgon2i) {
+                $string = password_hash($password, static::$algoCurrent, static::$optionsArgon2i);
+            } else {
+                if(mb_strlen($password) > 72)
+                {
+                    throw new Exception('Password too long');
+                }
+                $string = password_hash($password, static::$algoCurrent, static::$optionsBcrypt);
+            }
+        } catch (Exception $e) {
+            if($e->getMessage() == 'Password too long'){
+                throw new Exception('Password too long');
+            }
+            throw new Exception('Hash Failure');
+        }
+
+        return $string;
     }
 
     /**
@@ -47,7 +95,13 @@ class Crypt
      */
     public static function needsRehash(string $hash): bool
     {
-        return password_needs_rehash($hash, static::$algo, static::$options);
+        static::chooseAlgo();
+
+        if (static::$algoCurrent === static::$algoArgon2i) {
+            return password_needs_rehash($hash, static::$algoCurrent, static::$optionsArgon2i);
+        }
+
+        return password_needs_rehash($hash, static::$algoCurrent, static::$optionsBcrypt);
     }
 
     /**
@@ -67,44 +121,72 @@ class Crypt
         return $string;
     }
 
-    /*public static function encrypt($data)
-    {
-    }
-
-    public static function decrypt($data)
-    {
-    }*/
-
     /**
-     * @param int $value
+     * @param int $bytes Maximum memory (in bytes)
+     *
+     * @throws Exception
      */
-    public static function setOptionMemoryCost(int $value): void
+    public static function setOptionArgon2iMemoryCost(int $bytes): void
     {
-        static::$options['memory_cost'] = $value;
+        if ($bytes < 16) {
+            throw new Exception('Memory cost is too small');
+        }
+        static::$optionsArgon2i['memory_cost'] = $bytes;
     }
 
     /**
-     * @param int $value
+     * @param int $time Maximum amount of time
+     *
+     * @throws Exception
      */
-    public static function setOptionTimeCost(int $value): void
+    public static function setOptionArgon2iTimeCost(int $time): void
     {
-        static::$options['time_cost'] = $value;
+        if ($time < 1) {
+            throw new Exception('Time cost is outside of allowed time range');
+        }
+        static::$optionsArgon2i['time_cost'] = $time;
     }
 
     /**
-     * @param int $value
+     * @param int $threads Number of threads to use
+     *
+     * @throws Exception
      */
-    public static function setOptionThreads(int $value): void
+    public static function setOptionArgon2iThreads(int $threads): void
     {
-        static::$options['threads'] = $value;
+        if ($threads < 1) {
+            throw new Exception('Invalid number of threads');
+        }
+        static::$optionsArgon2i['threads'] = $threads;
     }
 
     /**
      * @return array
      */
-    public static function getOptions(): array
+    public static function getOptionsArgon2i(): array
     {
-        return static::$options;
+        return static::$optionsArgon2i;
+    }
+
+    /**
+     * @param int $rounds Number of round
+     *
+     * @throws Exception
+     */
+    public static function setOptionBcryptCost(int $rounds): void
+    {
+        if ($rounds < 4 || $rounds > 31) {
+            throw new Exception('Invalid number of rounds (between 4 and 31)');
+        }
+        static::$optionsBcrypt['cost'] = $rounds;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getOptionsBcrypt(): array
+    {
+        return static::$optionsBcrypt;
     }
 
     /**
@@ -121,5 +203,22 @@ class Crypt
     public static function getCaracters(): string
     {
         return static::$caracters;
+    }
+
+    private static function chooseAlgo(): void
+    {
+        if (static::$algoFixed) {
+            return;
+        }
+
+        static::$algoCurrent = defined('PASSWORD_ARGON2I') ? static::$algoArgon2i : static::$algoBcrypt;
+    }
+
+    /**
+     * @return int
+     */
+    public static function getCurrentAlgo(): int
+    {
+        return static::$algoCurrent;
     }
 }
